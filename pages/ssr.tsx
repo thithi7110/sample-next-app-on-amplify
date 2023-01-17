fdb4a1be83364ea17bd1bea71992befae1964b21
thithi7110 <thithi7110@gmail.com>
2023-01-17 22:50:44 +0900

    ファースト

---
+++ pages/ssr.tsx
@@ -0,0 +1,31 @@
+// pages/ssr.js
+
+import Link from "next/link";
+
+type propstype = {formattedDate:string}
+
+export default function SSR({ formattedDate }:propstype) {
+    return (
+      <>
+        <h1>Server-side rendered page</h1>
+        <p>
+          This page is server-side rendered. It was rendered on {formattedDate}.
+        </p>
+        <p>
+          <Link href="/">View a static page.</Link>
+        </p>
+      </>
+    );
+  }
+  
+  export async function getServerSideProps() {
+    const renderDate = Date.now();
+    const formattedDate = new Intl.DateTimeFormat("en-US", {
+      dateStyle: "long",
+      timeStyle: "long",
+    }).format(renderDate);
+    console.log(
+      `SSR ran on ${formattedDate}. This will be logged in CloudWatch.`
+    );
+    return { props: { formattedDate } };
+  }
